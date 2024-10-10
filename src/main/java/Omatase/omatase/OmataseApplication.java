ed63567035041a01b1cb675694e34b5df196b63b
jmsparrowk <jmsparrowk@naver.com>
2024-10-10 21:56:04 +0900

    10/10

---
+++ src/main/java/Omatase/omatase/OmataseApplication.java
@@ -0,0 +1,13 @@
+package Omatase.omatase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmataseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmataseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Omatase/omatase/OmataseApplicationTests.java
@@ -0,0 +1,13 @@
+package Omatase.omatase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmataseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
