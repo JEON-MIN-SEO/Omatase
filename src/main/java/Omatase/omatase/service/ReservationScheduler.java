3c5157e2b5bc523176c27bd71647b78b41ba28db
jmsparrowk <jmsparrowk@naver.com>
2024-11-04 10:41:17 +0900

    11/4

--- src/main/java/Omatase/omatase/service/ReservationScheduler.java
+++ src/main/java/Omatase/omatase/service/ReservationScheduler.java
@@ -18,14 +18,15 @@ public class ReservationScheduler {
         this.reservationRepository = reservationRepository;
     }
 
-    // 매 10분마다 예약 상태를 확인하고, 필요한 경우 CANCELED로 변경
-    @Scheduled(fixedRate = 3600000) // 10분마다 실행
+    // 매 1시간마다 예약 상태를 확인하고, 필요한 경우 CANCELED로 변경
+    @Scheduled(fixedRate = 3600000) // 1시간마다 실행
     public void checkAndCancelExpiredReservations() {
         List<ReservationEntity> reservations = reservationRepository.findAll();
 
         for (ReservationEntity reservation : reservations) {
+            // 상태가 AVAILABLE인 예약에 대해서만 확인
             if (reservation.getStatus() == ReservationStatus.AVAILABLE) {
-                // AVAILABLE 상태이고, 10분이 지난 경우
+                // 예약이 AVAILABLE 상태로 변경된 후 24시간이 지났는지 확인
                 if (reservation.getAvailable_date_time() != null &&
                         reservation.getModifiedAt().plusHours(24).isBefore(LocalDateTime.now())) {
 
